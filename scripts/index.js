d3d4660ca335c54a45093ce9d283b2eb9b5c43dd
The Quetapiton Dev <oamost123@gmail.com>
2025-10-08 17:41:46 -0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,4 @@
-let
+let favicon = document.getElementsByTagName("link")[0]
+
+
+document.getElementsByTagName("p")[1].innerHTML = `<img src='${favicon.href}' width=50'></img> EromadeiteBlox! A sua loja de roblox BARATA!`
